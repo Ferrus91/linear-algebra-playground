a4b2d73785dfaca9d80f3857b5950237185e276b
SamKeays <samskyscanner9@gmail.com>
2020-11-16 00:56:31 +0100

    tweaks

--- matrixMultiplication.js
+++ matrixMultiplication.js
@@ -2,12 +2,13 @@ const matrix1 = require(`./matrices/${process.argv[2]}`);
 const matrix2 = require(`./matrices/${process.argv[3]}`);
 
 const n = matrix1[0].length;
-if (!matrix2.length) throw Error('Matrix multiplication impossible')
+if (n != matrix2.length) throw Error('Matrix multiplication impossible')
+const m = matrix2[0].length;
 
 const productMatrix = [];
 for (let i = 0; i < n; i++) {
     const productMatrixRow = [];
-    for (let j = 0; j < n; j++) {
+    for (let j = 0; j < m; j++) {
         let sum = 0;
         for (let k = 0; k < n; k++) {
             sum += matrix1[i][k] * matrix2[k][j];
