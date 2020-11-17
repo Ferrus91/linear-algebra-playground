fb1e30f24f8a4a2090c9188acbd88d8635dcca17
SamKeays <samskyscanner9@gmail.com>
2020-11-17 22:30:10 +0100

    copy and transpose

---
+++ utils.js
@@ -0,0 +1,28 @@
+const genericCopy = (matrix, entrySelector) => {
+    const n = matrix.length;
+    const m = (matrix[0] || []).length;
+
+    copiedMatrix = [];
+    for (let i = 0; i < n; i += 1) {
+        const copiedRow = [];
+        for (let j = 0; j < m; j += 1) {
+            copiedRow.push(entrySelector(matrix, i, j));
+        }
+        copiedMatrix.push(copiedRow);
+    }
+    return copiedMatrix;
+}
+
+
+const deepCopy = (matrix) => {
+    return genericCopy(matrix, (matrix, i, j) => matrix[i][j]);
+}
+
+const transpose = (matrix) => {
+    return genericCopy(matrix, (matrix, i, j) => matrix[j][i]);
+}
+
+module.exports = {
+    deepCopy,
+    transpose,
+};
